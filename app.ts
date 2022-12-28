9fd3973b53c0c40c1c062290fb42595b79bcccbc
Rasec Camacho <rasec.camacho19@gmail.com>
2022-12-27 18:35:22 -0700

    ServerTS

--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-export const nombre = 'Rasec';
+import dotenv from 'dotenv';
+import Server from './models/server';
 
-console.log(nombre);
+// Configurar dot.env
+dotenv.config();
+
+const server = new Server();
+
+server.listen();
